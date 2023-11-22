24d9aadbb0f64ac882feb78663160417d217ddf3
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-11-22 12:09:01 -0300

    feat: Estruturar a página

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className='section-banner'>
+        <div className='container-info'>
+          <div className='borda-gradiente'>
+            <img src="./assets/neymar.jpg" alt="" title="" />
+          </div>
+          <h1 className='efeito-sombra'>Neymar Jr</h1>
+        </div>
+
+        <div className='container-links'>
+          <a className='links'  href="#teste">
+            <img src="./assets/youtube.png" alt="" title="" />
+            <p>Siga meu canal no Youtube</p>
+            </a>
+          <a className='links'  href="#teste">
+            <img src="./assets/instagram.png" alt="" title="" />
+            <p>Me siga no Instagram</p>
+            
+            </a>
+          <a className='links'  href="#teste">
+            <img src="./assets/twitter.png" alt="" title="" />
+            <p>Me siga no Twitter</p>
+            
+            </a>
+          <a className='links'  href="#teste">
+            <img src="./assets/facebook.png" alt="" title="" />
+            <p>Me siga no Facebook</p>
+            
+            </a>
+          <a className='links'  href="#teste">
+            <img src="./assets/twitch.png" alt="" title="" />
+            <p>Me siga no Twitch</p>
+            </a>
+        </div>
+      </section>
+      <section className='section-estatistica'>
+        <h2 className='efeito-sombra'>Estatísticas</h2>
+        <table className='tabela-estatistica'>
+          <tr className="tabela-primeira-coluna">
+            <td>Times</td>
+            <td>Temporadas</td>
+            <td>Jogos</td>
+            <td>Gols</td>
+            <td>Assistências</td>
+            <td>Títulos</td>
+          </tr>
+          <tr>
+            <td className='primeira-coluna'>
+              <img src="./logo192.png" alt="" title="" />
+              <p>Brasil</p>
+            </td>
+            <td>13</td>
+            <td>124</td>
+            <td>77</td>
+            <td>77</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td className='primeira-coluna'>
+              <img src="./logo192.png" alt="" title="" />
+              <p>Santos</p>
+            </td>
+            <td>5</td>
+            <td>230</td>
+            <td>138</td>
+            <td>77</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td className='primeira-coluna'>
+              <img src="./logo192.png" alt="" title="" />
+              <p>Barcelona</p>
+            </td>
+            <td>4</td>
+            <td>186</td>
+            <td>105</td>
+            <td>77</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td className='primeira-coluna'>
+              <img src="./logo192.png" alt="" title="" />
+              <p>PSG</p>
+            </td>
+            <td>6</td>
+            <td>173</td>
+            <td>118</td>
+            <td>77</td>
+            <td>13</td>
+          </tr>
+          <tr>
+            <td className='primeira-coluna'>
+              <img src="./logo192.png" alt="" title="" />
+              <p>Al-Hilal</p>
+            </td>
+            <td>1</td>
+            <td>0</td>
+            <td>0</td>
+            <td>77</td>
+            <td>0</td>
+          </tr>
+        </table>
+      </section>
     </div>
   );
 }
